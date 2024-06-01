abb1ead06f22c59df36afebb33c30e19f1e85ee5
brohithkr <brohithkr@gmail.com>
2024-06-02 00:13:51 +0530

    complete base pdf

---
+++ src/components/document.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Page, Text, View, Document } from "@react-pdf/renderer";
+import { Form } from "./types";
+import styles from "./documentStyle";
+
+// Create Document Component
+const MyDocument = ({form}: {form: Form}) => (
+  <Document>
+    <Page style={styles.container}>
+      <View>
+        <Text style={styles.heading}>Biographical Details</Text>
+        <View style={styles.table}>
+          {/* <View style={[styles.tableRow, styles.tableHeader]}>
+            <Text style={styles.tableCol}>Field</Text>
+            <Text style={styles.tableCol}>Value</Text>
+          </View> */}
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Name:</Text>
+            <Text style={styles.tableCol}>{form.name}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Date of Birth:</Text>
+            <Text style={styles.tableCol}>{form.dob}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Date of Assessment:</Text>
+            <Text style={styles.tableCol}>{form.dateOfAssesment}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Gender:</Text>
+            <Text style={styles.tableCol}>{form.gender}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>School:</Text>
+            <Text style={styles.tableCol}>{form.school}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Grade:</Text>
+            <Text style={styles.tableCol}>{form.grade}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Parents:</Text>
+            <Text style={styles.tableCol}>{form.parents}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Parent's Email:</Text>
+            <Text style={styles.tableCol}>{form.parentsEmail}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCol}>Home Address:</Text>
+            <Text style={styles.tableCol}>{form.homeAddress}</Text>
+          </View>
+
+          {/* Add the rest of the details */}
+        </View>
+      </View>
+    </Page>
+  </Document>
+);
+
+export default MyDocument;
---
+++ src/components/documentStyle.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        padding: 10,
+        border: '1 solid #ccc',
+      },
+      heading: {
+        fontSize: 21,
+        backgroundColor: '#d9d9d9',
+        padding: 5,
+        fontFamily: 'Helvetica-Bold',
+        marginBottom: 10,
+        textAlign: 'center',
+        fontWeight: 'ultrabold'
+      },
+      table: {
+        display: 'table',
+        width: 'auto',
+        borderStyle: 'solid',
+        borderColor: '#fff',
+        borderWidth: 1,
+        borderRightWidth: 0,
+        borderBottomWidth: 0,
+      },
+      tableRow: {
+        flexDirection: 'row',
+      },
+      tableCol: {
+        flex: 1,
+        borderStyle: 'solid',
+        borderColor: '#fff',
+        borderBottomWidth: 1,
+        borderRightWidth: 1,
+        padding: 5,
+      },
+      tableHeader: {
+        backgroundColor: '#f0f0f0',
+        fontFamily: 'Helvetica-Bold',
+      },
+  });
+
+export default styles;
