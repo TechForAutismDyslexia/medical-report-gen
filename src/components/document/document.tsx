802f2151b17ee6ac43a10118d708b85efdf12eaf
brohithkr <brohithkr@gmail.com>
2024-07-21 19:53:22 +0530

    fix page order

--- src/components/document/document.tsx
+++ src/components/document/document.tsx
@@ -1,16 +1,16 @@
-import { Document } from '@react-pdf/renderer';
+import { Document } from "@react-pdf/renderer";
 
-import Pages from './pages/pages';
+import Pages from "./pages/pages";
 
 export default function MyDocument() {
   return (
     <Document>
-      <Pages.Page5 />
-      <Pages.Page4 />
       <Pages.Page1 />
-      <Pages.Page6 />
       <Pages.Page2 />
       <Pages.Page3 />
+      <Pages.Page4 />
+      <Pages.Page5 />
+      <Pages.Page6 />
     </Document>
   );
 }
